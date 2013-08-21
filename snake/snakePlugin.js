8f99e89ae9420f8db61a512cd78c5118561c9f25
Jenich <J.Zhivitsa@gmail.com>
2013-08-21 14:55:35 +0300

    debug

--- snake/snakePlugin.js
+++ snake/snakePlugin.js
@@ -93,8 +93,8 @@
 				ch1 = 2, ch2 = 2;
 
 			if ( !self.isFirstAcceleration ) {
-				
-				if ( self.snake.length ) {alert( ch1 + ' ' + ch2 );
+				alert( self.snake.length );
+				if ( self.snake.length ) {
 					ch1 = self.snake[0][0] - self.snake[1][0];
 					ch2 = self.snake[0][1] - self.snake[1][1];
 				}
